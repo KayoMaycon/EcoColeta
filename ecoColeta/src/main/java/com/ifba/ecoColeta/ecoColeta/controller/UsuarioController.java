970e64082c59e21c0d0d8bca5678a4fb28775e32
Kayo Maycon <102869922+KayoMaycon@users.noreply.github.com>
2023-09-15 14:27:35 -0300

    add

--- ecoColeta/src/main/java/com/ifba/ecoColeta/ecoColeta/controller/UsuarioController.java
+++ ecoColeta/src/main/java/com/ifba/ecoColeta/ecoColeta/controller/UsuarioController.java
@@ -2,11 +2,9 @@ package com.ifba.ecoColeta.ecoColeta.controller;
 
 
 import com.ifba.ecoColeta.ecoColeta.model.Usuario;
-import com.ifba.ecoColeta.ecoColeta.service.UsuarioService;
 import com.ifba.ecoColeta.ecoColeta.service.UsuarioServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
