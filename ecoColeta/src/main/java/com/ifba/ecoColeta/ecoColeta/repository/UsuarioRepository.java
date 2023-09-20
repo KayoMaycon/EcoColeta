dd5de7da94b8455aa242baef7e33d020ec0ce1dc
Joeziojr <joeziojunior@gmail.com>
2023-09-20 10:00:51 -0300

    teste

--- ecoColeta/src/main/java/com/ifba/ecoColeta/ecoColeta/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ifba.ecoColeta.ecoColeta.repository;
-
-import com.ifba.ecoColeta.ecoColeta.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
-
-
-}
