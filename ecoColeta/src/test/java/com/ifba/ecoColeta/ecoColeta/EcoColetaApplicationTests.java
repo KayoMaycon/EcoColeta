9fa93ed3399e2d843f4b413125f521f1a42055a1
Joeziojr <127506251+Joeziojr@users.noreply.github.com>
2023-09-20 21:52:26 -0300

    Add files via upload

---
+++ ecoColeta/src/test/java/com/ifba/ecoColeta/ecoColeta/EcoColetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifba.ecoColeta.ecoColeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoColetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
